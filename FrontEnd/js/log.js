4ca07bf20ae3254ba049b792798073c3a3baceda
Xaarty <caillaud.mikael.pro@gmail.com>
2023-12-28 17:17:03 +0100

    Réalisation de l'autentification

--- FrontEnd/js/log.js
+++ FrontEnd/js/log.js
@@ -87,6 +87,59 @@ loginForm.addEventListener("submit", async (event) => {
 
 console.log("fichier chargé");
 
+async function getIds(email, password) {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password })
+    });
+    if (!response.ok) {
+        throw Error ('Erreur dans l’identifiant ou le mot de passe')
+    }
+    const ids = await response.json();
+    return ids;
+}
+
+async function log() {
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    console.log(email)
+    console.log(password)
+
+    try {
+        const id = await getIds(email, password);
+        console.log(id);
+        localStorage.setItem('loggedIn', 'true')
+        localStorage.setItem('userId', id.userId)
+        localStorage.setItem('token', id.token)
+        window.location.href='index.html'
+    } catch (error) {
+        console.error('Erreur lors de la connexion:', error.message);
+        passwordInput.placeholder = 'Erreur dans l’identifiant ou le mot de passe'
+        passwordInput.value = ''
+        passwordInput.style.border = '1px solid red'  
+    }
+}
+
+const loginForm = document.getElementById("login_form");
+const passwordInput = document.getElementById("password");
+
+passwordInput.addEventListener("click", (event) => {
+    if (event.button === 0) {
+        passwordInput.style.border = 'none'
+    }})
+
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("Formulaire soumis");
+    log();
+});
+/*
 async function getIds() {
     try {
         console.log("Envoi de la requête...");
@@ -128,4 +181,4 @@ loginForm.addEventListener("submit", async (event) => {
     } catch (error) {
         console.error('Erreur lors de la connexion:', error.message);
     }
-});
+});*/
